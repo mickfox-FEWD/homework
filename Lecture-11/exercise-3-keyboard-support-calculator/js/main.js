fa4a29de8c43f6707a73afc72582dc76132ea09e
mickfox-FEWD <github@mickfox.com>
2025-02-16 23:39:17 +0000

    Ex3 Calculator done

--- Lecture-11/exercise-3-keyboard-support-calculator/js/main.js
+++ Lecture-11/exercise-3-keyboard-support-calculator/js/main.js
@@ -7,13 +7,18 @@
   let resultDisplay = false;
 
   function appendToDisplay(value) {
-    if (currentDisplay === "0" || resultDisplay) {
-      currentDisplay = value;
-    } else {
-      currentDisplay += value;
+    console.log("appendToDisplay: " + value);
+    if (value != "Shift") {
+      console.log("not shift");
+
+      if (currentDisplay === "0" || resultDisplay) {
+        currentDisplay = value;
+      } else {
+        currentDisplay += value;
+      }
+      resultDisplay = false;
+      updateDisplay();
     }
-    resultDisplay = false;
-    updateDisplay();
   }
 
   function updateDisplay() {
@@ -46,9 +51,13 @@
     currentDisplay = "0";
     updateDisplay();
   }
+
   function init() {
+    console.log("init");
+
     window.addEventListener("load", (event) => {
       calcButtons.addEventListener("click", (event) => {
+        console.log("click event: " + event.target.textContent);
         if (event.target.tagName.toLowerCase() === "button") {
           switch (event.target.textContent) {
             case "CE":
@@ -67,6 +76,44 @@
         }
       });
     });
+
+    window.addEventListener("keydown", function (event) {
+      // console.log("Key pressed:", event.key + "type " + typeof event.key);
+      let modKey = event.key;
+      // if (event.shiftKey) {
+      //   if (event.key == "=") {
+      //     modKey = "+";
+      //   } else if (event.key == "8") {
+      //     modKey = "*";
+      //   }
+      // }
+      switch (modKey) {
+        case "Backspace":
+          clearLastElement();
+          break;
+        case "c":
+          clearDisplay();
+          break;
+        case "=":
+          calculateResult();
+          break;
+        case "Enter":
+          calculateResult();
+          break;
+        // case "+":
+        //   break;
+
+        // case "*":
+        //   break;
+
+        default:
+          // pass value of the clicked button to appendToDisplay()
+          // if (!event.shiftKey) {
+          appendToDisplay(modKey);
+        // }
+      }
+    });
   }
+
   init();
 })();
